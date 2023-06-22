3fb861ad0d018684c89c1fc3922520889286eb4b
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-22 14:51:40 +0530

    Update streamlite_app.py
    added data frames

--- streamlite_app.py
+++ streamlite_app.py
@@ -7,3 +7,6 @@ st.text('🥣Omega 3 & Blueberry Oatmeal')
 st.text(' 🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 toast')
+import pandas
+fruit_list=pandas.read.cvs("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(fruit_list)
