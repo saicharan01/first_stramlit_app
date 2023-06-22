f83fda5615a519c259bb4ebc343770dafc8ad30b
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-22 14:48:27 +0530

    Update streamlite_app.py
    added break fast menu

--- streamlite_app.py
+++ streamlite_app.py
@@ -2,3 +2,8 @@ import streamlit as st
 st.title("i am using snowflake")
 st.header(' snowflake')
 st.title('data warehouse')
+st.header('Breakfast Menu')
+st.text('🥣Omega 3 & Blueberry Oatmeal')
+st.text(' 🥗Kale, Spinach & Rocket Smoothie')
+st.text('🐔Hard-Boiled Free-Range Egg')
+  st.text.('🥑🍞 toast')
