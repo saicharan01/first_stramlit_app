847a637b644a18aa335ff81f07d5753e8a1e79b1
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-22 14:55:02 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -8,5 +8,5 @@ st.text(' 🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 toast')
 import pandas
-fruit_list=pandas.read_cvs("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.dataframe(fruit_list)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
