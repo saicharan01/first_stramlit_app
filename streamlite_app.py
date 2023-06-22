68fb380afe2c34603ca3f3a14908a1c923426a62
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-22 14:43:44 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,24 +1,2 @@
-
-import snowflake.connector
-conn = snowflake.connector.connect(
-    user='SAICHARAN11',
-    password='Saicharan@11',
-    account='bc34544.central-india.azure',
-    warehouse='DEMO_WH',
-    database='MY_DB',
-    schema='MY_SCH'
-)
-
-
-
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM SALES")
-results = cursor.fetchall()
-
-
-
-import streamlit as st
-st.write(results)
-
-
-
+import streamlit
+streamlit.title("i am using snowflake)
