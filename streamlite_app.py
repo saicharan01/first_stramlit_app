276eb555c5e8a3dbda7ad914886013fe79191dce
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-23 11:46:55 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -10,7 +10,10 @@ st.text('🥑🍞 toast')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
 st.dataframe(my_fruit_list)
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruit_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_show=my_fruit_list.loc[fruit_selected]
+st.dataframe(fruits_show)
 
 
