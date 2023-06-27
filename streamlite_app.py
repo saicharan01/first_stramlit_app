5bcd7c387e0164f01c2437161253977372c86f60
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-27 13:31:23 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,35 +1,35 @@
-import streamlit as st
-st.title("i am using snowflake")
-st.header(' snowflake')
-st.title('data warehouse')
-st.header('Breakfast Menu')
-st.text('🥣Omega 3 & Blueberry Oatmeal')
-st.text(' 🥗Kale, Spinach & Rocket Smoothie')
-st.text('🐔Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 toast')
+import streamlit 
+streamlit .title("i am using snowflake")
+streamlit .header(' snowflake')
+streamlit .title('data warehouse')
+streamlit .header('Breakfast Menu')
+streamlit .text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit .text(' 🥗Kale, Spinach & Rocket Smoothie')
+streamlit .text('🐔Hard-Boiled Free-Range Egg')
+streamlit .text('🥑🍞 toast')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.dataframe(my_fruit_list)
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruit_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Banana'])
+streamlit .dataframe(my_fruit_list)
+streamlit .multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruit_selected=streamlit .multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Banana'])
 fruits_show=my_fruit_list.loc[fruit_selected]
-st.dataframe(fruits_show)
+streamlit .dataframe(fruits_show)
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+fruit_choice = streamlit .text_input('What fruit would you like information about?','Kiwi')
+streamlit .write('The user entered ', fruit_choice)
 import requests as rs
-st.header("Fruityvice Fruit Advice!")
+streamlit .header("Fruityvice Fruit Advice!")
 fruityvice_response = rs.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+streamlit .text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-st.dataframe(fruityvice_normalized)
+streamlit .dataframe(fruityvice_normalized)
 
 import snowflake.connector
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
