fb0ae0fc0f7d2438576b56c5d39de257987a533a
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-28 11:35:05 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,21 +1,21 @@
-import streamlit 
+import streamlit as st
 import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLError
 def get_fruitvice_data(this_fruit_choise):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choise ) 
-  streamlit.text(fruityvice_response.json())
+  st.text(fruityvice_response.json())
   fruityvice_normalize=pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalize
 
-streamlit.header("Frutiyvice fruit advice:")
+st.header("Frutiyvice fruit advice:")
 try:
-  fruit_choice = streamlit .text_input('What fruit would you like information about?')
+  fruit_choice = st .text_input('What fruit would you like information about?')
   if not  fruit_choice :
-    streamlit.error("please select a fruit to get information.")
+    st.error("please select a fruit to get information.")
   else:
     function_retun_value=get_fruitvice_data(fruit_choice)
-    streamlit.dataframe(function_retun_value)
+    st.dataframe(function_retun_value)
 except URLError as e:
-  streamlit.error()
+  st.error()
