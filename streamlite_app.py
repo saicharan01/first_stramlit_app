74082416b97ad1d2d7b1dede4fb974215922b462
saicharan01 <137391888+saicharan01@users.noreply.github.com>
2023-06-28 11:57:36 +0530

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -1,34 +1,33 @@
 import streamlit as st
-import pandas
+import pandas as pd
 import requests
 import snowflake.connector
 from urllib.error import URLError
-def get_fruitvice_data(this_fruit_choise):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choise ) 
-  fruityvice_normalize=pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalize
 
-streamlit.header("Frutiyvice fruit advice:")
+def get_fruitvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalize = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalize
+
+st.header("Fruityvice fruit advice:")
 try:
-  fruit_choice = streamlit .text_input('What fruit would you like information about?')
-  if not  fruit_choice :
-    st.error("please select a fruit to get information.")
-  else:
-    function_retun_value=get_fruitvice_data(fruit_choice)
-    st.dataframe(function_retun_value)
+    fruit_choice = st.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        st.error("Please select a fruit to get information.")
+    else:
+        function_return_value = get_fruitvice_data(fruit_choice)
+        st.dataframe(function_return_value)
 except URLError as e:
-  st.error()
+    st.error("An error occurred: " + str(e))
 
 st.header("The fruit load list:")
 
-def get_fruit_load_list():
-  my_cur = my_cnx.cursor()
-  my_cur.execute("SELECT * FROM fruit_load_list")
-  return my_cur.fetchall()
+def get_fruit_load_list(connection):
+    with connection.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM fruit_load_list")
+        return my_cur.fetchall()
 
 if st.button("Get fruit load list"):
-  my_cux = snowflake.connector.connect(**st.secrets["snowflake"])
-  my_data_rows = get_fruit_load_list()
-  st.dataframe(my_data_rows)
-
-  
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list(my_cnx)
+    st.dataframe(my_data_rows)
